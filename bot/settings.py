fc4141c256c1b956029a6d90b7d5a1ab015f3dba
Biswajit Paloi <bpaloi5555@gmail.com>
2025-05-16 11:02:41 +0530

    test

--- bot/settings.py
+++ bot/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bot.urls'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 
 
 TEMPLATES = [
